c87fab7c8a28cc09d8a20f17a784ab5d8dce435b
danielmartinez-maker <daniel.martinez@lomasaltas.edu.mx>
2026-09-02 12:30:02 -0600

    feat(cpp-ui): complete alpha placement tool previews

--- cpp/engine/include/civic/presentation/NativeTools.hpp
+++ cpp/engine/include/civic/presentation/NativeTools.hpp
@@ -16,7 +16,11 @@ enum class NativeTool : std::uint8_t {
     Road,
     Zone,
     Facility,
+    Utility,
+    Service,
+    TransitStop,
     Transit,
+    Bulldoze,
 };
 
 struct RoadToolDraft {
@@ -34,12 +38,40 @@ struct FacilityToolDraft {
     std::string facility_type;
 };
 
+struct UtilityToolDraft {
+    Point2 position{};
+    std::string utility_type;
+};
+
+struct ServiceToolDraft {
+    Point2 position{};
+    std::string service_type;
+};
+
+struct TransitStopToolDraft {
+    Point2 position{};
+    TransitStopKind kind{TransitStopKind::BusStop};
+};
+
 struct TransitToolDraft {
     std::vector<std::string> stop_ids;
     VehicleKind mode{VehicleKind::Bus};
 };
 
-using NativeToolDraft = std::variant<std::monostate, RoadToolDraft, ZoneToolDraft, FacilityToolDraft, TransitToolDraft>;
+struct BulldozeToolDraft {
+    Point2 position{};
+};
+
+using NativeToolDraft = std::variant<
+    std::monostate,
+    RoadToolDraft,
+    ZoneToolDraft,
+    FacilityToolDraft,
+    UtilityToolDraft,
+    ServiceToolDraft,
+    TransitStopToolDraft,
+    TransitToolDraft,
+    BulldozeToolDraft>;
 
 class NativeToolWorkflow {
 public:
@@ -50,13 +82,17 @@ public:
     [[nodiscard]] std::expected<void, std::string> previewRoad(std::vector<Point2> path, RoadClass road_class);
     [[nodiscard]] std::expected<void, std::string> previewZone(std::string parcel_id, std::string zoning_code);
     [[nodiscard]] std::expected<void, std::string> previewFacility(Point2 position, std::string facility_type);
+    [[nodiscard]] std::expected<void, std::string> previewUtility(Point2 position, std::string utility_type);
+    [[nodiscard]] std::expected<void, std::string> previewService(Point2 position, std::string service_type);
+    [[nodiscard]] std::expected<void, std::string> previewTransitStop(Point2 position, TransitStopKind kind);
     [[nodiscard]] std::expected<void, std::string> previewTransit(std::vector<std::string> stop_ids, VehicleKind mode);
+    [[nodiscard]] std::expected<void, std::string> previewBulldoze(Point2 position);
     [[nodiscard]] std::expected<void, std::string> commit(NativeUiController& controller);
     void cancel() noexcept;
 
 private:
     NativeTool active_tool_{NativeTool::Inspect};
-    ToolPreviewState preview_{.tool_id = "inspect"};
+    ToolPreviewState preview_{.tool_id = "inspect", .geometry = {}};
     NativeToolDraft draft_{};
 };
 
