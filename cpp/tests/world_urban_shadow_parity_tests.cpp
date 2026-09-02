2b263205746ed3c36ec7a47ae83c4516de11c15b
danielmartinez-maker <daniel.martinez@lomasaltas.edu.mx>
2026-09-01 20:24:57 -0600

    test(cpp-urban): add shadow parity RED contract

---
+++ cpp/tests/world_urban_shadow_parity_tests.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+TEST(WorldUrbanShadowParityRed, SharedFixtureRunnerIsRequired) {
+  FAIL() << "Task 18 RED: shared TS/native shadow fixture runner is not implemented";
+}
+
+TEST(WorldUrbanShadowParityRed, FirstDivergenceReducerIsRequired) {
+  FAIL() << "Task 18 RED: first-divergence reducer is not implemented";
+}
