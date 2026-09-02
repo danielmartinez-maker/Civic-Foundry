ba45fecdbd80073fc4512808afd2771d709fd09d
danielmartinez-maker <daniel.martinez@lomasaltas.edu.mx>
2026-09-01 23:15:23 -0600

    feat(native-urban): expose authority through C ABI

--- cpp/engine/include/civic/bridge/civic_engine.h
+++ cpp/engine/include/civic/bridge/civic_engine.h
@@ -50,6 +50,9 @@ CF_API cf_error_code cf_engine_create_world(cf_engine* engine, const uint8_t* da
 CF_API cf_error_code cf_engine_restore_world(cf_engine* engine, const uint8_t* data, size_t size, cf_buffer* out_buffer);
 CF_API cf_error_code cf_engine_create_legacy_world(cf_engine* engine, const uint8_t* data, size_t size, cf_buffer* out_buffer);
 CF_API cf_error_code cf_engine_run_design_storm(cf_engine* engine, const uint8_t* data, size_t size, cf_buffer* out_buffer);
+CF_API cf_error_code cf_engine_rebuild_urban_legacy(cf_engine* engine, const uint8_t* data, size_t size, cf_buffer* out_buffer);
+CF_API cf_error_code cf_engine_restore_urban_state(cf_engine* engine, const uint8_t* data, size_t size, cf_buffer* out_buffer);
+CF_API cf_error_code cf_engine_get_urban_snapshot(cf_engine* engine, cf_buffer* out_buffer);
 CF_API cf_error_code cf_engine_get_last_error(cf_engine* engine, cf_error* out_error);
 CF_API void cf_buffer_free(cf_buffer buffer);
 
